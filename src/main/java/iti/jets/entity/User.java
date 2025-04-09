5d3a688c3ed5d5d10cc84b3d577a2905960fda62
Ahmed Ashraf <ahmedashraf8228@gmail.com>
2025-04-09 23:01:24 +0200

    aa

--- src/main/java/iti/jets/entity/User.java
+++ src/main/java/iti/jets/entity/User.java
@@ -4,15 +4,12 @@ import iti.jets.service.PasswordUtils;
 import jakarta.persistence.*;
 import java.io.Serializable;
 import java.sql.Date;
-
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import java.util.Date;
 import java.util.List;
 
 @Entity
